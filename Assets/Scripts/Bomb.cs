c30c89f628fcc7472eb8367f91a683f173d793d6
lukasz19944 <lukasz19944@gmail.com>
2018-10-30 23:00:45 +0100

    Pierwsza wersja gry po ukończeniu tutorialu

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+
+    public GameObject explosionPrefab;
+    private bool exploded = false;
+    public LayerMask levelMask;
+
+	// Use this for initialization
+	void Start () {
+        Invoke("Explode", 3f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void Explode() {
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+
+        StartCoroutine(CreateExplosions(Vector3.forward));
+        StartCoroutine(CreateExplosions(Vector3.right));
+        StartCoroutine(CreateExplosions(Vector3.back));
+        StartCoroutine(CreateExplosions(Vector3.left));
+
+        GetComponent<MeshRenderer>().enabled = false;
+        exploded = true;
+        transform.Find("Collider").gameObject.SetActive(false);
+        Destroy(gameObject, 0.3f);
+    }
+
+    private IEnumerator CreateExplosions(Vector3 direction) {
+        for (int i = 1; i < 3; i++) {
+            RaycastHit hit;
+            Physics.Raycast(transform.position + new Vector3(0, 0.5f, 0), direction, out hit, i, levelMask);
+
+            if (!hit.collider) {
+                Instantiate(explosionPrefab, transform.position + (i * direction), explosionPrefab.transform.rotation);
+            } else {
+                break;
+            }
+
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (!exploded && other.CompareTag("Explosion")) {
+            CancelInvoke("Explode");
+            Explode();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Tutorial-Bomberman3D
 Bomberman 3D zrobiony w Unity na podstawie tutorialu ze strony raywenderlich.com
+
+![Gra](/Screenshots/game.png?raw=true)
+
+## Informacje
+- Gra dla dwóch graczy
+- Wybuchająca bomba wytwarza ogień o długości dwóch jednostek w każdą stronę, chyba że napotka przeszkodę
+- Ogień zabija gracza
+- Wybuchająca bomba powoduje wybuch innej bomby w obrębie wybuchu
+- Wyznaczanie zwyciężcy lub remisu
+
+## Do zrobienia
+- Limit bomb dla gracza
+- Interfejs użytkownika
+- Bloki możliwe do zniszczenia bombą
+- Powerupy
+- Życia gracza
+- Zablokowanie możliwości stawiania kilku bomb w jednym miejscu
+
+## Źródło
+https://www.raywenderlich.com/244-how-to-make-a-game-like-bomberman-with-unity
